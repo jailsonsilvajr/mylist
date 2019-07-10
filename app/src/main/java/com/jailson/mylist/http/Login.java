633c43c78fb6808b00b23d529735dd88b818b2cf
Jailson Silva <jjsj2@cin.ufpe.br>
2019-07-10 06:31:58 -0300

    Class to login and to User created

---
+++ app/src/main/java/com/jailson/mylist/http/Login.java
@@ -0,0 +1,87 @@
+package com.jailson.mylist.http;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.google.gson.Gson;
+import com.jailson.mylist.object.User;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Login extends AsyncTask<Void, Void, User> {
+
+    private String url;
+    private String email;
+    private String password;
+    private ProgressBar progressBar;
+
+    public Login(String url, String email, String password, ProgressBar progressBar){
+
+        this.url = url;
+        this.email = email;
+        this.password = password;
+        this.progressBar = progressBar;
+    }
+
+    @Override
+    protected User doInBackground(Void... voids) {
+
+        try {
+
+            URL link = new URL(this.url);
+            HttpURLConnection connection = (HttpURLConnection) link.openConnection();
+
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setDoOutput(true);
+
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("email", this.email);
+            jsonObject.put("password", this.password);
+
+            PrintStream printStream = new PrintStream(connection.getOutputStream());
+            printStream.println(jsonObject.toString());
+
+            connection.connect();
+
+            if(connection.getResponseCode() == 200){
+
+                String userJsonStr;
+                Scanner scanner = new Scanner(link.openStream());
+                userJsonStr = scanner.next();
+                while(scanner.hasNext()){
+
+                    userJsonStr += " ";
+                    userJsonStr += scanner.next();
+                }
+
+                Gson gson = new Gson();
+                User user = gson.fromJson(userJsonStr, User.class);
+
+                return user;
+            }
+
+        } catch (MalformedURLException e) {
+
+            Log.i("Login", e.getMessage());
+        } catch (IOException e) {
+
+            Log.i("Login", e.getMessage());
+        } catch (JSONException e) {
+
+            Log.i("Login", e.getMessage());
+        }
+
+        return null;
+    }
+}
---
+++ app/src/main/java/com/jailson/mylist/object/User.java
@@ -0,0 +1,8 @@
+package com.jailson.mylist.object;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+
+}
