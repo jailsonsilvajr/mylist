8027ae456934c186b63a516f8e3bedc8222af733
Jailson Silva <jjsj2@cin.ufpe.br>
2019-07-11 08:50:35 -0300

    xml da tela de login

--- app/src/main/java/com/jailson/mylist/activity/LoginActivity.java
+++ app/src/main/java/com/jailson/mylist/activity/LoginActivity.java
@@ -2,14 +2,21 @@ package com.jailson.mylist.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.jailson.mylist.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.progressBar = findViewById(R.id.pbLogin_enter);
+        this.progressBar.setVisibility(View.GONE);
     }
 }
